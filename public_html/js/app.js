a12ef87c50220b82be0226850db30d0898023393
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-09-25 14:51:24 +0300

    Update app.js

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,42 +1,46 @@
-angular.module("toDoList", []).controller("toDoListCtrl", ['$scope', '$http', function($scope, $http){
+angular.module("toDoApp", []).controller("toDoAppCtrl", ['$scope', '$http', function($scope, $http){
   
     $http.get('tasks.json').success(function(data){
       $scope.taskList = data;
     });
 
     $scope.taskList = angular.fromJson(localStorage.getItem('taskList'));
-    $scope.order = 'task';
+    
     
     $scope.addTask = function(){
-var t = {
-          completed : false,
-          task : $scope.newTask,
+        $scope.taskList.push({
+          title : $scope.newTaskTitle,
           desc : $scope.newTaskDescription, 
-          important : false
+        });
 
-        }
-        $scope.taskList.push(t);
         $scope.save();
-        $scope.newTask = '';
-        $scope.newTaskDescription='';
+        $scope.newTaskTitle = '';
+        $scope.newTaskDescription = '';
             };
     
     $scope.removeCompleted = function(){
         var oldList = $scope.taskList;
         $scope.taskList = [];
-        angular.forEach(oldList, function(task, desc){
-           if(!task.completed){
-            $scope.taskList.push(task);
-             $scope.taskList.push(desc);
+        angular.forEach(oldList, function(x){
+           if(!x){
+            $scope.taskList.push(x);
            }
         });
         $scope.save();
     };
+
+      $scope.remove = function() {
+        var oldList = $scope.taskList;
+        $scope.taskList = [];
+        taskList.remove();
+    };
     
     $scope.save = function(){
+
+      
         localStorage.setItem('taskList', angular.toJson($scope.taskList));
         $http.post('app.php', angular.toJson($scope.taskList)).then(function(){
-          // log success
+           $scope.taskList = response.data.records;
         });
     }
 
